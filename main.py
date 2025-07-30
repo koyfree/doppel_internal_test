47daf0f0ca966cbc2cdf67c19de8fb9d5fb967d8
koyfree <koyfree@gmail.com>
2025-07-30 12:35:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ if st.session_state.step == "select":
         # st.markdown("#### 🧾 당신의 프로필")
         # st.code(profiles[user_name])
 
-        chatbot_type = st.radio("챗봇 유형을 선택하세요:", ["도플갱어 챗봇", "일반 챗봇"])
+        chatbot_type = st.radio("챗봇 유형을 선택하세요:", ["도플갱어 챗봇"#, "일반 챗봇"])
+                                                 ]
         topic = st.radio("대화 주제를 선택하세요:", ["정신 건강", "관계 갈등"])
-        model = st.radio("모델을 선택하세요:", ["GPT-4.1", "Claude Sonnet 4", "Gemini 2.5 Flash"])
+        model = st.radio("모델을 선택하세요:", ["GPT-4.1"#, "Claude Sonnet 4", "Gemini 2.5 Flash"])
+                                       ]
 
         if st.button("다음"):
             st.session_state.update({
@@ -48,10 +50,10 @@ if st.session_state.step == "select":
 elif st.session_state.step == "chat":  
     if st.session_state.model == "GPT-4.1":
         import chatbot_gpt as app
-    elif st.session_state.model == "Claude Sonnet 4":
-        import chatbot_claude as app
-    elif st.session_state.model == "Gemini 2.5 Flash":
-        import chatbot_gemini as app
+    #elif st.session_state.model == "Claude Sonnet 4":
+        #import chatbot_claude as app
+    #elif st.session_state.model == "Gemini 2.5 Flash":
+        #import chatbot_gemini as app
 
     app.run(
         user_name=st.session_state.user_name,
